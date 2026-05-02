0f66565f6534545b86f51ed4da23a448bd17437c
tofazzolshah <tofazzolhaqueshah@gmail.com>
2026-05-02 23:20:41 +0600

    this is my anothee code

---
+++ 05_practice.js
@@ -0,0 +1,23 @@
+console.log("this is my practice file for JS");
+
+//1
+let a="Alice";
+console.log(a+2);// this is call concatenation in JS.
+//2
+console.log(typeof a);
+//3
+const item= {
+    name: "Tofazzol",
+    age: 27
+}
+item["friends"]= ["Kutub", "Mizan"];
+//item=34; // This will throw an error because item is a constant variable.
+console.log(item);
+
+//4
+const dic={
+    "good": "valo",
+    "bad": "kharap",
+    "friend": "bondhu"
+}
+console.log(dic["good"]);
