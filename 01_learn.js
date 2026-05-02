213e0b21cc2095815bf5b563e8e5dea6f93a0c4a
tofazzolshah <tofazzolhaqueshah@gmail.com>
2026-05-02 22:29:25 +0600

    update

--- 01_learn.js
+++ 01_learn.js
@@ -2,3 +2,10 @@ console.log("Hello JS");
 
  let a = 23;
  console.log(a);
+
+ //how to define a function in JS
+ function addThreeNubers(a, b, c) {
+     return a + b + c;
+ }
+
+ console.log(addThreeNubers(1, 2, 3));
