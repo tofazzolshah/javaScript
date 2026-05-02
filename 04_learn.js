1489be77ffdd5f5cc2230a173718e226e4d82d8f
tofazzolshah <tofazzolhaqueshah@gmail.com>
2026-05-02 22:55:44 +0600

    new file

---
+++ 04_learn.js
@@ -0,0 +1,21 @@
+let a=null;
+let b= 145;
+let c = true;
+let d=BigInt("1234567890123456789012345678901234567890");
+let e=Symbol("I am a nice symbol");
+let f=undefined;
+let g="Tofazzol";
+
+
+console.log(a, b, c, d, e, f,g);
+console.log(typeof d);
+
+//object in JS
+//it's like dictionary in python
+let item = {
+     "Tofazzol": 27,
+     "kutub": "student",
+     "Mizan": true,
+        "friends": ["Tofazzol", "Kutub", "Mizan"]
+}
+console.log(item ["kutub"]);
