a6237973b0128de0170b8516b421a9feba62846d
tofazzolshah <tofazzolhaqueshah@gmail.com>
2026-05-11 00:45:02 +0600

    new

---
+++ 13_learn.js
@@ -0,0 +1,17 @@
+//This is my string file
+
+let name="tofazzol";
+console.log(name.length);
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+console.log(name.slice(0,4));
+console.log(name.slice(4));
+
+let boy1="tofazzol";
+let boy2="mizan";
+
+let add= `${boy1} and ${boy2} are best friends.`;
+console.log(add);
+//Escape sequence
+let text="I am learning JavaScript.\nI am enjoying it.";
+console.log(text);
