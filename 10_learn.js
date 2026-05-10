e447a4689094c073765bf03df156dc3ca24832b6
tofazzolshah <tofazzolhaqueshah@gmail.com>
2026-05-10 21:54:46 +0600

    hello

---
+++ 10_learn.js
@@ -0,0 +1,18 @@
+//While loop in JavaScript
+
+let a=5;
+let i =0;
+while(i<a){
+    console.log(i);
+    i++;
+}
+
+// do while loop in JavaScript
+console.log("do while loop");
+let j=0;
+let b=3;
+
+do{
+    console.log(j);
+    j++;
+}while(j<b);
