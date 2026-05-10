14e05a5ba44b6d45cdac79d2b9959e74e730c809
tofazzolshah <tofazzolhaqueshah@gmail.com>
2026-05-11 00:55:45 +0600

    new

---
+++ 14_learn.js
@@ -0,0 +1,13 @@
+ let Myname="tofazzol";
+ console.log(Myname.length);
+console.log(Myname.toUpperCase());
+console.log(Myname.toLowerCase());
+console.log(Myname.slice(0,4));
+console.log(Myname.slice(4));
+
+console.log(Myname.replace("tof","mof"));//eita replace kore dibe but original string change hobe na
+
+let friend ="rafi";
+console.log(Myname.concat(" is a friend of ",friend)); 
+let friend2="    Pagla    ";
+console.log(friend2.trim());//eita string er shuru ar sesh theke space remove kore dibe
