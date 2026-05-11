05f33e65809eacd2ea91b75653b15f54338f5397
tofazzolshah <tofazzolhaqueshah@gmail.com>
2026-05-12 00:21:21 +0600

    hello

---
+++ 16_learn.js
@@ -0,0 +1,15 @@
+// today i will learn about array methods
+
+let a=[14,72,35,47,75,false,true,"tofazzol"];
+console.log(a);
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log("The length of array is :", a.length)
+
+a[3]=77;
+console.log(a[3]) // changing a value of an array
+
+console.log(typeof a)
+
